dd424013fb1b5a7d374025b60213a3e87e315953
Mohamed Naji <130527900+najimohamed@users.noreply.github.com>
2024-02-22 22:17:39 +0100

    Delete php ecommerce directory

--- php ecommerce/productdelete.php
+++
@@ -1,7 +0,0 @@
-<?php   
-require_once 'include/database.php';
-var_dump($_GET);
-$sqlState = $pdo->prepare('DELETE FROM product WHERE id=?');
-$delete = $sqlState->execute([$id]);
-header('location: productlist.php');
-
